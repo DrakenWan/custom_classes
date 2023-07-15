504fe28b53c93d47462314da3af9e4d26b5d69c2
Draken Wan <kartikaykaul13@gmail.com>
2023-07-15 01:41:36 -0700

    Update date.py
    Added a feature to print date with month names instead of numbers

--- date.py
+++ date.py
@@ -1,16 +1,32 @@
 ### custom date class
 class date:
+    months = ['default','January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October'\
+             ,'November', 'December']
     
-    def __init__(self,day=0,month=0,year=0,format='%d/%m/%y', ce=True):
+    def __init__(self,day=0,month=0,year=0,format='%d/%m/%y', word=False):
         self.day, self.month, self.year = self.validate(day,month,year)
         self.format = format
-        self.ce = ce
+        self.word = word
         
         self.evaluate_date()
     
+    
+    def __dayh(self, digit):
+        if int(digit) == 1:
+            return 'st'
+        elif int(digit) == 2:
+            return 'nd'
+        elif int(digit) == 3:
+            return 'rd'
+        else:
+            return 'th'
+    
     def __str__(self):
+        
         if self.day == 0 and self.month == 0 and self.year == 0:
             return '0 CE'
+        if self.word:
+            return self.months[int(self.month)] + ' ' + self.day + self.__dayh(str(self.day)[-1]) + ', ' + self.year
         return self.day + self.format[2] + self.month + self.format[5] +self.year
     
     def validate(self,dd,mm,yy):
@@ -24,7 +40,7 @@ class date:
             raise ValueError('Invalid month value. Length exceeded.')
         
         if len(str(yy)) < 4:
-            raise ValueError('Invalid day value. Length exceeded.')
+            raise ValueError('Invalid year value. Length exceeded.')
             
         max1=0
         if(mm==1 or mm==3 or mm==5 or mm==7 or mm==8 or mm==10 or mm==12):
@@ -52,5 +68,5 @@ class date:
             self.month = '0' + self.month
     #def to_datetime(self,string, format='%d%m%y'):
 
-##x = date(29,2,1992, format='%d.%m.%y')
+##x = date(29,2,1992, format='%d.%m.%y', word=True)
 ##print(x)
